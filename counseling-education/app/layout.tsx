506372be3d923796082afd9172ec7f28b7feed9b
azerose <woojjang8671@naver.com>
2025-06-01 15:26:33 +0900

    [Publish] change favicon

--- counseling-education/app/layout.tsx
+++ counseling-education/app/layout.tsx
@@ -7,6 +7,13 @@ export const metadata: Metadata = {
     title: 'NGEC',
     description: 'Created with v0',
     generator: 'v0.dev',
+    icons: {
+        icon: [
+            { url: '/NGEC_logo_green.png', sizes: '32x32' },
+            { url: '/NGEC_logo_green.png', sizes: '48x48' },
+            { url: '/NGEC_logo_green.png', sizes: '96x96' },
+        ],
+    },
 };
 
 export default function RootLayout({
