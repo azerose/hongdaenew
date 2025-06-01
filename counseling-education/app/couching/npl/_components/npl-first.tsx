c4d4055fffa058fcff9830db0a4a17f76e7d6c78
azerose <woojjang8671@naver.com>
2025-06-01 16:21:00 +0900

    [Publish] publishing the npl test page

---
+++ counseling-education/app/couching/npl/_components/npl-first.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+
+const NplFirst = () => {
+    return (
+        <div>
+            <section className="w-full flex items-center justify-center  py-10 bg-gradient-to-r from-teal-50 to-blue-50">
+                <div className="container px-4 md:px-6">
+                    <div className="grid  lg:grid-cols-2  justify-center items-center">
+                        <div className="mx-auto grid max-w-5xl mt-12">
+                            <div className="w-[500px] h-[550px] relative">
+                                <Image
+                                    src="/npl_main.jpg"
+                                    alt="npl 검사"
+                                    fill
+                                    priority
+                                    className="object-cover rounded-lg"
+                                />
+                            </div>
+                        </div>
+                        <div className="space-y-4">
+                            <h1 className="text-3xl  font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                                NLP프로그램 (Neuro Linguistic Programming)
+                            </h1>
+                            <p className="max-w-[600px] pt-4 text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                                인간의 뇌, 언어, 행동 간의 상호작용을 이해하고 개선하는 데 초점을
+                                맞춘 강력한 자기 발전 도구입니다.
+                                <br />
+                                코칭센터에서 제공하는 NLP프로그램은 부정적인 사고 패턴을 변화시키고,
+                                목표 달성을 위한 내적 자원을 발굴하여 실질적인 성과를 이루도록
+                                돕습니다.
+                                <br /> NLP는 우리가 사용하는 언어와 신경학적 과정, 그리고 그것들이
+                                우리의 행동에 미치는 영향을 탐구함으로써, 보다 효율적이고 긍정적인
+                                변화를 일으킬 수 있는 기회를 제공합니다.
+                                <br /> 코칭은 개인의 한계를 넘어설 수 있도록 돕고, 새로운 사고의
+                                틀을 만들어가며, 더 나은 선택을 할 수 있도록 유도합니다.
+                                <br /> 이 과정을 통해 스스로의 잠재력을 깨우고, 삶의 변화에 NLP
+                                프로그램은 직장 내에서의 성과 향상, 대인 관계 개선, 개인적 목표 달성
+                                등 다양한 분야에서 강력한 변화를 일으킬 수 있습니다.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+export default NplFirst;
---
+++ counseling-education/app/couching/npl/_components/npl-second.tsx
@@ -0,0 +1,59 @@
+const NplSecond = () => {
+    return (
+        <section className="w-full flex flex-col gap-12 items-center justify-center py-8  bg-white">
+            <div className="flex flex-col gap-3 max-w-5xl w-full">
+                <div className="text-3xl font-bold tracking-tighter">추천 대상</div>
+                <div className="text-xl flex flex-col   md:gap-3">
+                    <div className="flex gap-10 md:gap-10">
+                        <span>1. 반복되는 부정적인 사고 패턴을 끊고 싶은 사람 </span>
+                        <span>2. 내적 장애물(무의식적 패턴)을 극복하고 싶은 사람</span>
+                    </div>
+                    <div className=" flex gap-10 md:gap-10">
+                        <span>3. 커뮤니케이션 능력을 향상시키고 싶은 사람</span>
+                        <span>4. 자아 성장과 발전을 위한 도전적인 사람 </span>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-3 max-w-5xl w-full">
+                <div className="text-3xl font-bold tracking-tighter">코칭 이후의 달라질 모습</div>
+                <div className="text-xl font-semibold">
+                    <div className="font-bold">1. 부정적인 사고 패턴 변화</div>
+                </div>
+                <div className="text-xl">
+                    무의식적으로 반복되던 부정적인 사고 방식을 변화시켜, 보다 긍정적이고 건설적인
+                    사고로 인지의 흐름과 패턴을 전환할 수 있습니다.
+                </div>
+                <div className="text-xl font-semibold">
+                    <div className="font-bold">2. 목표 달성을 통한 자신감 회복</div>
+                </div>
+                <div className="text-xl">
+                    목표에 대한 명확한 비전과 그에 따른 실천 계획을 세울 수 있으며, 내적인 장벽을
+                    넘어 목표 달성에 대한 자신감을 얻을 수 있습니다. 
+                </div>
+                <div className="text-xl font-semibold">
+                    <div className="font-bold">3. 선택의 확신과 내적 동기 부여 향상</div>
+                </div>
+                <div className="text-xl">
+                    진로에 대한 불확실성에서 벗어나 자신감 있게 직업적인 방향을 설정하고, 이를 위한
+                    구체적인 계획을 실행에 옮길 수 있게 됩니다. 
+                </div>
+                <div className="text-xl font-semibold">
+                    <div className="font-bold">3. 효과적인 커뮤니케이션 능력 향상 </div>
+                </div>
+                <div className="text-xl">
+                    타인과의 관계에서 더욱 명확하고 효과적으로 의사소통할 수 있게 되어, 대인
+                    관계에서 발생하는 갈등을 줄이고 협력을 증진시킬 수 있습니다. 
+                </div>
+                <div className="text-xl font-semibold">
+                    <div className="font-bold">4. 자기 발전과 직무성과 향상 </div>
+                </div>
+                <div className="text-xl">
+                    NLP 기술을 활용하여 자신의 한계를 극복하고, 일상 생활에서의 생산성과 성과를 눈에
+                    띄게 향상시킬 수 있습니다.
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default NplSecond;
